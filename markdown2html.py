d06643ce0894b4b870f1a1c4475f37164ff7e8c5
Alex Jarabi <jarabialex@gmail.com>
2023-12-17 11:42:42 +0300

    Script to parse paragraph syntax for generating HTML

--- markdown2html.py
+++ markdown2html.py
@@ -10,6 +10,8 @@ def create_heading(line, htmlfile):
     """
     Parses Headings Markdown syntax for generating HTML
     """
+    # Remove leading/trailing spaces
+    line = line.strip()
     count = 0
 
     while count < len(line) and line[count] == '#':
@@ -41,21 +43,36 @@ if __name__ == "__main__":
         with open(html_file, 'a') as htmlfile:
             # Flag to track opening and closing of list items
             list_started = False
+
+            # Flag to track opening and closing of paragraph
+            paragraph_started = False
+
+            # Holds either OL or UL
             list_type = ''
 
             for line in mdfile:
-                # Remove leading/trailing spaces
-                line = line.strip()
-
                 if line.startswith('#'):
                     # If there is an open list, close it first
                     if list_started:
                         htmlfile.write(f'</{list_type}>\n')
                         list_started = False
 
+                    # If there is an open paragraph, close it first
+                    if paragraph_started:
+                        htmlfile.write('</p>\n')
+                        paragraph_started = False
+
                     create_heading(line, htmlfile)
 
                 elif line.startswith('- ') or line.startswith('* '):
+                    # If there is an open paragraph, close it first
+                    if paragraph_started:
+                        htmlfile.write('</p>\n')
+                        paragraph_started = False
+                    
+                    # Remove leading/trailing spaces
+                    line = line.strip()
+
                     if line.startswith('- '):
                         list_type = 'ul'
                     elif line.startswith('* '):
@@ -69,5 +86,27 @@ if __name__ == "__main__":
 
                     htmlfile.write(f'<li>{ultext}</li>\n')
 
+                elif (line and line[0].isalpha()) or line.isspace():
+                    # If there is an open list, close it first
+                    if list_started:
+                        htmlfile.write(f'</{list_type}>\n')
+                        list_started = False
+
+                    if not paragraph_started:
+                        if not line.isspace():
+                            paragraph_started = True
+                            htmlfile.write('<p>\n')
+                    elif paragraph_started:
+                        if line.isspace():
+                            htmlfile.write('\n</p>\n')
+                            paragraph_started = False
+                        else:
+                            htmlfile.write('\n<br/>\n')
+                    line = line.strip()
+                    htmlfile.write(line)
+
             if list_started:
                 htmlfile.write(f'</{list_type}>\n')
+
+            if paragraph_started:
+                htmlfile.write('\n</p>\n')
